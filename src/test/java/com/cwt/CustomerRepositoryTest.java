70e46fd68fa14919a91effc54c28e063841ad59f
sh3yabr1ol <sheilamaeabriol12@gmail.com>
2022-03-11 11:31:33 +0800

    testing

--- src/test/java/com/cwt/CustomerRepositoryTest.java
+++ src/test/java/com/cwt/CustomerRepositoryTest.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 

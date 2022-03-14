b169a17575c12f83ae78e41b80d25c5e358cdf0f
sh3yabr1ol <sheilamaeabriol12@gmail.com>
2022-03-14 16:46:55 +0800

    added spring security

--- src/main/java/com/cwt/config/SecurityConfig.java
+++ src/main/java/com/cwt/config/SecurityConfig.java
@@ -1,20 +1,38 @@
 package com.cwt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/create").hasRole("USER").and().formLogin();
+		httpSecurity
+        //HTTP Basic authentication
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/create").hasRole("ADMIN")
+        .antMatchers(HttpMethod.PUT, "/update/*").hasRole("ADMIN")
+        .antMatchers(HttpMethod.DELETE, "/delete/*").hasRole("ADMIN")
+        .antMatchers(HttpMethod.PATCH, "/updatePartial/*").hasRole("ADMIN")
+        .antMatchers(HttpMethod.GET, "/all").permitAll()
+        .and()
+        .csrf().disable();
 	}
 	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER").and().withUser("admin1").password("admin1").roles("USER", "ADMIN");
+		auth.inMemoryAuthentication()
+        .withUser("user").password("{noop}user").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
+
 	}
 }
--- src/test/java/com/cwt/CustomerControllerTests.java
+++ src/test/java/com/cwt/CustomerControllerTests.java
@@ -1,17 +1,24 @@
 package com.cwt;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -22,8 +29,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.cwt.entities.Customer;
 import com.cwt.service.CustomerService;
@@ -39,11 +54,27 @@ public class CustomerControllerTests {
     @MockBean
     CustomerService customerService;
     
+    @Autowired
+    WebApplicationContext context;
+    
     private Customer customer;
+    
+    private TestRestTemplate restTemplate;
+    
+    private URL base;
+ 
+    
+    public void setUpSecurity(){
+    	mockMvc = MockMvcBuilders
+  	          .webAppContextSetup(context)
+  	          .apply(springSecurity())
+  	          .build();
+    }
     
     @BeforeEach
     public void setUp() {
     	customer = new Customer(1, "John", "Doee", "jd@g.com", "QC");
+    	setUpSecurity();
     }
     
     @Nested
@@ -88,6 +119,7 @@ public class CustomerControllerTests {
         void createCustomer_success() throws Exception {
         	
         	mockMvc.perform(post("/customers/create")
+        			  .with(httpBasic("admin", "admin"))
         		      .content(new ObjectMapper().writeValueAsString(customer))
         		      .contentType(MediaType.APPLICATION_JSON)
         		      .accept(MediaType.APPLICATION_JSON))
@@ -107,6 +139,7 @@ public class CustomerControllerTests {
             Mockito.when(customerService.updateCustomerRecord(customer.getCustId(), toUpdateCustomer)).thenReturn(toUpdateCustomer);
             
         	mockMvc.perform(put("/customers/update/{custId}", customer.getCustId())
+        			  .with(httpBasic("admin", "admin"))
         		      .content(new ObjectMapper().writeValueAsString(toUpdateCustomer))
         		      .contentType(MediaType.APPLICATION_JSON)
         		      .accept(MediaType.APPLICATION_JSON))
@@ -123,12 +156,61 @@ public class CustomerControllerTests {
         	Mockito.doNothing().when(customerService).deleteCustomerRecord(customer.getCustId());
         	
         	mockMvc.perform(delete("/customers/delete/{custId}", customer.getCustId())
+        			.with(httpBasic("admin", "admin"))
         		      .contentType(MediaType.APPLICATION_JSON)
         		      .accept(MediaType.APPLICATION_JSON))
         		      .andExpect(status().isAccepted());       
         }
+        
     }
     
+    
+    @Nested
+    @DisplayName("Nested Test For Authentication Failure")
+    class NesterTestedForAuthFailure{
+    	
+    	 @Test
+         @DisplayName("Test for Customer Deletion Wrong Authentication")
+         void deleteCustomer_fail() throws Exception {
+         	
+         	Mockito.doNothing().when(customerService).deleteCustomerRecord(customer.getCustId());
+         	
+         	mockMvc.perform(delete("/customers/delete/{custId}", customer.getCustId())
+         			  .with(httpBasic("admin", "wrongpassword"))
+         		      .contentType(MediaType.APPLICATION_JSON)
+         		      .accept(MediaType.APPLICATION_JSON))
+         		      .andExpect(status().isUnauthorized());       
+         }
+    	 
+    	 @Test
+         @DisplayName("Test for Customer Update Wrong Authentication")
+         void updateCustomer_failed() throws Exception {
+         	
+         	Customer toUpdateCustomer = new Customer(1, "John2", "Doe2", "jd2@g.com", "QC");
+     	
+             Mockito.when(customerService.updateCustomerRecord(customer.getCustId(), toUpdateCustomer)).thenReturn(toUpdateCustomer);
+             
+         	mockMvc.perform(put("/customers/update/{custId}", customer.getCustId())
+         			  .with(httpBasic("admin", "wrongpassword"))
+         		      .content(new ObjectMapper().writeValueAsString(toUpdateCustomer))
+         		      .contentType(MediaType.APPLICATION_JSON)
+         		      .accept(MediaType.APPLICATION_JSON))
+         		      .andExpect(status().isUnauthorized());       
+         }
+    	 
+    	 @Test
+         @DisplayName("Test for Customer Creation Wrong Authentication")
+         void createCustomer_failed() throws Exception {
+         	
+         	mockMvc.perform(post("/customers/create")
+         			  .with(httpBasic("admin", "wrongpassword"))
+         		      .content(new ObjectMapper().writeValueAsString(customer))
+         		      .contentType(MediaType.APPLICATION_JSON)
+         		      .accept(MediaType.APPLICATION_JSON))
+         		      .andExpect(status().isUnauthorized());
+         		      
+         }
+    }
     
     @Nested
 	@DisplayName("Nested Test For All Validation Scenarios")
@@ -182,6 +264,5 @@ public class CustomerControllerTests {
         		      .andExpect(jsonPath("$.email", is("Please input valid email format")));	      
         }
     }
-    
     
 }
